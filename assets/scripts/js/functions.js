481caba0f7c1295e3007449574f64f5f1a226e2b
Szymon Ratajczyk <furusenpai@gmail.com>
2016-09-10 01:05:53 +0200

    changes

--- assets/scripts/js/functions.js
+++ assets/scripts/js/functions.js
@@ -33,7 +33,7 @@ function navigation() {
 
 function nightMode() {
 
-  var trigger = "#drop>#triggerNight, .mobile-trigger>#triggerNight",
+  var trigger = "#drop>#triggerNight",
       elements = ".body-custom, .logo, .navigation-bar, #drop, .data, .title, .description, footer, .article, .article-body",
       elementS = "html, .body-custom";
 
